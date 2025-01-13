ef6cab4ad25003bb04daed14217bd63cc6c3d69f
Aranda27A <abraham.aranda.lu27@gmail.com>
2025-01-13 13:29:27 -0600

    Add semantic order for Backend and Creation of Server w express

--- Full-Static-Website/index.js
+++ Full-Static-Website/index.js
@@ -6,10 +6,6 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
-
-
-
-
 const app = express();
 const port = 3000
 
@@ -17,12 +13,27 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get( "/", (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/views/index.html');
   });
-  
-  
+
+app.get( "/Discografia", (req, res) => {
+    res.sendFile(__dirname + '/views/Discografia.html');
+  });
+app.get("/Contacto", (req, res) => {
+  res.sendFile(__dirname + '/views/Contact.html');
+});
+app.get("/Videos", (req, res) => {
+  res.sendFile(__dirname + '/views/videos.html');
+});
+app.get("/Galeria", (req, res) => {
+  res.sendFile(__dirname + '/views/galeria.html');
+});
 
 
-  app.listen(port, (req, res)=> {
+
+
+
+
+app.listen(port, (req, res)=> {
     console.log("You have a port in 3000");
   })
